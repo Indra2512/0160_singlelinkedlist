2bf858f22898c445404bfe3c880498b26b77904d
Indra2512 <mononeon1225@gmail.com>
2025-05-08 09:59:13 +0700

    menambahkan Class

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -2,3 +2,28 @@
 #include <string.h>
 using namespace std;
 
+class Node
+{
+    public:
+    int noMhs;
+    Node *next;
+};
+
+class list
+{
+    Node *START;
+
+    public:
+    list()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+    }
+
+}
